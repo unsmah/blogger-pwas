f1be664119bf3e9c518f49b39a932becbb063248
unsmah <42187761+unsmah@users.noreply.github.com>
2025-09-17 04:26:00 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "blogger-pwa-v1";
+const urlsToCache = [
+  "/",
+  "/p/testtt.html"
+];
+
+// Install service worker
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch from cache if offline
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
